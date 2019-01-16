6ce10a2d29ee99962f209eaa0dbd276e3c9761d7
zhuzhengjun <zhengjun_zhu@hotmail.com>
2019-01-16 15:53:52 +0800

    reverse 2ms

---
+++ src/com/sn/leetcode/dp/intf/ReverseString.java
@@ -0,0 +1,23 @@
+package com.sn.leetcode.dp.intf;
+
+/**
+ * @author 18073758
+ * Created by zhuzhengjun on 2019/1/16.
+ */
+public interface ReverseString {
+    /**
+     * 编写一个函数，其作用是将输入的字符串反转过来。
+     *
+     * 示例 1:
+     *
+     * 输入: "hello"
+     * 输出: "olleh"
+     * 示例 2:
+     *
+     * 输入: "A man, a plan, a canal: Panama"
+     * 输出: "amanaP :lanac a ,nalp a ,nam A"
+     * @param s
+     * @return
+     */
+    String reverseString(String s);
+}
